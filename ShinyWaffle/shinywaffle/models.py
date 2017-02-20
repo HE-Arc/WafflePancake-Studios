e7a971c1d4bbfc451f46520eff7720e8bacdb775
Julien M'Poy <julien.mpoy@gmail.com>
2017-02-20 14:36:04 +0100

    Setup testing models for use in the administration web UI

--- ShinyWaffle/shinywaffle/models.py
+++ ShinyWaffle/shinywaffle/models.py
@@ -5,7 +5,13 @@ class Gallery(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.title
+
 
 class Image(models.Model):
     gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
