66f5b38177590a986d06146c7f8517ad3131cba8
Julien M'Poy <julien.mpoy@gmail.com>
2017-03-11 11:43:22 +0100

    Created image model and added author field to the the gallery model

--- shinywaffle/shinywaffle/galleries/models.py
+++ shinywaffle/shinywaffle/galleries/models.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.contrib.auth.models import AbstractUser
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+from ..users.models import User
 
 
 @python_2_unicode_compatible
@@ -13,10 +13,26 @@ class Gallery(models.Model):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    title = models.CharField(_('Title of Gallery'), blank=False, max_length=255)
+    title = models.CharField(
+        _('Title of Gallery'), blank=False, max_length=255)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('gallery:detail', kwargs={'title': self.title})
+
+
+class Image(models.Model):
+    title = models.CharField(_('Title of Image'), blank=False, max_length=255)
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    file = models.ImageField(
+        upload_to='pictures/', default='pictures/None/no-img.jpg')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('images:detail', kwargs={'title': self.title})
