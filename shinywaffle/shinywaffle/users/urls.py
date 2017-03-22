ebc6d6c221218bdc21f69ddbb7a3517076a943b9
joel-vaucher <joel.vaucher@he-arc.ch>
2017-03-22 15:53:45 +0100

    affichage des amis d'un utilisateur sur le lien friends

--- shinywaffle/shinywaffle/users/urls.py
+++ shinywaffle/shinywaffle/users/urls.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, unicode_literals
 from django.conf.urls import url
 
 from . import views
+from friendship.views import view_friends
 
 urlpatterns = [
     url(
@@ -25,5 +26,11 @@ urlpatterns = [
         regex=r'^~update/$',
         view=views.UserUpdateView.as_view(),
         name='update'
+    ),
+    url(
+        regex=r'^friendlist/(?P<username>[\w-]+)/$',
+        view=view_friends,
+        kwargs={'template_name' : 'users/user_friendlist.html'},
+        name='friendlist'
     ),
 ]
--- shinywaffle/shinywaffle/users/views.py
+++ shinywaffle/shinywaffle/users/views.py
@@ -4,6 +4,8 @@ from __future__ import absolute_import, unicode_literals
 from django.core.urlresolvers import reverse
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
 
+from django.contrib.auth.models import User
+from friendship.models import Friend, Follow
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import User
