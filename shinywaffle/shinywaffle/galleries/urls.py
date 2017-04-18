cd41b9dbe7078f92788f3ded57d48f0880bc37c9
joel-vaucher <joel.vaucher@he-arc.ch>
2017-04-18 17:37:53 +0200

    affichage des galeries personnelles

--- shinywaffle/shinywaffle/galleries/urls.py
+++ shinywaffle/shinywaffle/galleries/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     # ),
     url(regex=r'^$', view=views.gallery_list_view,
         name='index'),
+    url(regex=r'^(?P<username>[\w.@+-]+)/$', view=views.gallery_userlist_view,
+        name='userlist'),
     url(regex=r'^new/$', view=views.gallery_new_form_view,
         name='new'),
     # url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
@@ -21,7 +23,7 @@ urlpatterns = [
     #     view=views.UserRedirectView.as_view(),
     #     name='redirect'
     # ),
-    url(regex=r'^(?P<pk>\d)/$',
+    url(regex=r'^display/(?P<pk>\d)/$',
         view=views.GalleryDetailView.as_view(),
         name='detail'),
     url(regex=r'^edit/(?P<pk>\d)/$',
--- shinywaffle/shinywaffle/galleries/views.py
+++ shinywaffle/shinywaffle/galleries/views.py
@@ -3,15 +3,18 @@ from __future__ import absolute_import, unicode_literals
 
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from .models import Gallery
 
 from django.contrib.auth.decorators import login_required
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import GalleryNewForm, GalleryEditForm
 
+
+User = get_user_model()
 
 @login_required
 def gallery_list_view(request):
@@ -22,6 +25,11 @@ def gallery_list_view(request):
 class GalleryDetailView(LoginRequiredMixin, DetailView):
     model = Gallery
 
+@login_required
+def gallery_userlist_view(request, username):
+    users = User.objects.get(username=username)
+    galleries = Gallery.objects.filter(author_id=users.id)
+    return render(request, 'galleries/gallery_list.html', {'galleries_list': galleries})
 
 @login_required
 def gallery_new_form_view(request):
